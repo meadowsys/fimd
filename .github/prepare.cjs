aedacedcd8127e45ddd4ac89464727bf02ef812c
Meadowsys <blazeykirin@gmail.com>
2023-05-18 16:44:26 -0700

    safety check h

--- .github/prepare.cjs
+++ .github/prepare.cjs
@@ -1,6 +1,11 @@
 const { writeFileSync } = require("fs");
 const { resolve } = require("path");
 
+if (!process.env.VERSION) {
+	console.error("process.env.VERSION not set, please set it");
+	process.exit(1);
+}
+
 const pkg = require("../package.json");
 pkg.version = process.env.VERSION;
 
