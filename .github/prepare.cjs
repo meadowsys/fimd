e61e0a7b18fb9556ccffe0d5308a67dee3ebdbca
Meadowsys <blazeykirin@gmail.com>
2023-12-06 13:09:35 -0800

    another fix and some refactor
    theres always a little something you forget huh, this time its license and readme

--- .github/prepare.cjs
+++ .github/prepare.cjs
@@ -1,5 +1,5 @@
-const { writeFileSync } = require("fs");
-const { resolve } = require("path");
+const fs = require("fs");
+const path = require("path");
 
 if (!process.env.VERSION) {
 	console.error("process.env.VERSION not set, please set it");
@@ -9,4 +9,15 @@ if (!process.env.VERSION) {
 const pkg = require("../js/package.json");
 pkg.version = process.env.VERSION;
 
-writeFileSync(resolve(__dirname, "../js/package.json"), Buffer.from(JSON.stringify(pkg, null, "\t") + "\n"));
+fs.writeFileSync(path.resolve(__dirname, "../js/package.json"), Buffer.from(JSON.stringify(pkg, null, "\t") + "\n"));
+
+fs.writeFileSync(path.resolve(__dirname, "../js/.npmrc"), "//registry.npmjs.org/:_authToken=${NPM_TOKEN}\n");
+
+fs.copyFileSync(
+	path.resolve(__dirname, "../README.md"),
+	path.resolve(__dirname, "../js/README.md")
+);
+fs.copyFileSync(
+	path.resolve(__dirname, "../LICENSE"),
+	path.resolve(__dirname, "../js/LICENSE")
+);
